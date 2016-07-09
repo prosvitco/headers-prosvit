22db6887bdd4130f978a649d8acd129c902cd6c9
Volodymyr Danylyuk <danich19@yahoo.com>
2016-07-09 12:02:42 -0700

    Inital

---
+++ templates/header-relative.php
@@ -0,0 +1,36 @@
+<header role="banner" class="header-section section-dark navbar-default">
+    <div class="container">
+        <div class="row">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                    <span class="sr-only">
+                    <?= __('Toggle navigation', 'sage'); ?></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>           
+                    <?php $logo_image = get_field('logo_image', 'option'); ?>
+                    <?php if( !empty($logo_image) ): ?>
+                        <a class="navbar-logo navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+                            <img src="<?php echo $logo_image['url']; ?>" alt="<?php echo $logo_image['alt']; ?>" class="img-responsive" />
+                        </a>                    
+                    <?php endif; ?>
+            </div>
+            <nav id="navigation" class="navigation navbar-collapse collapse">
+
+                <?php 
+                if (has_nav_menu('primary_navigation')) :
+                    wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav navbar-center menu-nav']);
+                endif; 
+                ?>
+                <?php 
+                if (has_nav_menu('navbar-right-menu')) :
+                    wp_nav_menu(['theme_location' => 'navbar-right-menu', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav navbar-right menu-nav']);
+                endif; 
+                ?>
+
+                <div class="social-icons hidden-xs"></div>
+            </nav>
+        </div>
+    </div>
+</header>
